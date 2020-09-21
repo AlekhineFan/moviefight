abbf299a5ea4788d2b33f966ec1228785d9d9699
Mark Szavin <szavinm@yahoo.com>
2020-09-21 12:14:44 +0200

    create and separate utils

---
+++ utils.js
@@ -0,0 +1,11 @@
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args); //calls the function with all the spreaded arguments
+    }, delay);
+  };
+};
